4dc55654a6d8350953dc48d1141f85f0f9a3c16b
Andj <andjc@users.noreply.github.com>
2023-10-19 16:42:12 +1100

    Bumped version number; removed analyse form __init__/.py

--- el_internationalisation/__init__.py
+++ el_internationalisation/__init__.py
@@ -4,14 +4,13 @@ el_internationalisation
 Helper functions to improve Python internationalization.
 """
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 __author__ = 'Andrew Cunningham'
 __credits__ = 'Enabling Languages'
 
 from .digits import *
 from .ustrings import *
 from .bidi import *
-from .analyse import *
 from .transliteration_data import *
 from .transliteration import *
 from .utilities import *
@@ -20,7 +19,6 @@ from .bibliographic import *
 del(digits)
 del(ustrings)
 del(bidi)
-del(analyse)
 del(transliteration_data)
 del(transliteration)
 del(utilities)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='el_internationalisation',
-    version='0.4.4',
+    version='0.4.5 ',
     description='Helper functions to improve Python internationalisation',
     url='https://github.com/enabling-languages/el_internationalisation',
     author='Andrew Cunningham',
