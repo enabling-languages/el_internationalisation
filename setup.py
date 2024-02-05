1c6300233c3499d4ebd56b3e82d8adeeda9fc30d
Andj <andjc@users.noreply.github.com>
2024-02-05 11:29:51 +1100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='el_internationalisation',
-    version='0.5.14',
+    version='0.5.15',
     description='Helper functions to improve Python internationalisation',
     url='https://github.com/enabling-languages/el_internationalisation',
     author='Andrew Cunningham',
